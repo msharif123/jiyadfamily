b2a87ca649502c3e31d739fb98534d4888f053d7
msharif123 <mohamed.sharif.abdiwahab@gmail.com>
2025-07-17 20:08:29 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./components/Home"
+
+ 
+const  App=() =>{
+  
+
+  return (<>
+    <h1>jiyad -app</h1>
+    <Home/>
+    
+ </> )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+const Home =()=>{
+    return(
+        <h1>Jiyad Family</h1>
+    )
+}
+
+export default Home
